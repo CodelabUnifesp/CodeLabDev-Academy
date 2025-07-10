f6be100c9ef410310f994e200199483c130d744b
Livia Canuto <liviacanuto01@gmail.com>
2025-07-10 20:57:28 -0300

    feat: add lesson 5

--- frontend/src/data/courses/html-course/html.ts
+++ frontend/src/data/courses/html-course/html.ts
@@ -3,13 +3,14 @@ import lesson1 from './lesson1'
 import lesson2 from './lesson2'
 import lesson3 from './lesson3'
 import lesson4 from './lesson4'
+import lesson5 from './lesson5'
 
 
 const htmlCourse: Course = {
   slug: 'html',
   name: 'HTML',
   time: '1 hora',
-  lessons: [lesson1, lesson2, lesson3, lesson4]
+  lessons: [lesson1, lesson2, lesson3, lesson4, lesson5]
 }
 
 export default htmlCourse
