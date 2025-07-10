384a38ed97db01454fa3ccf0f7ca80fce2d70524
Livia Canuto <liviacanuto01@gmail.com>
2025-07-10 13:50:16 -0300

    feat: add lesson 4

--- frontend/src/data/courses/git-github/git-github.ts
+++ frontend/src/data/courses/git-github/git-github.ts
@@ -1,13 +1,14 @@
 import type { Course } from '../types'
 import lesson1 from './lesson1'
 import lesson2 from './lesson2'
+import lesson3 from './lesson3'
 
 
 const gitCourse: Course = {
   slug: 'git_github',
   name: 'Git e Github',
   time: '1 hora',
-  lessons: [lesson1, lesson2]
+  lessons: [lesson1, lesson2, lesson3]
 }
 
 export default gitCourse
